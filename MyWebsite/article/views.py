bcdabcc39831cfc04369cdd639e727545864c582
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2020-06-26 09:25:19 +0800

    文章目录

--- MyWebsite/article/views.py
+++ MyWebsite/article/views.py
@@ -58,16 +58,22 @@ def article_detail(request, id):
     print(article.updated)
     # 将markdown语法渲染成html样式
     # 欠缺：代码没有行号
-    article.body = markdown.markdown(article.body, extensions=[
-        # 包含 缩写、表格等常用扩展
-        'markdown.extensions.extra',
-        # 语法高亮扩展
-        'markdown.extensions.codehilite',
-    ])
+    md = markdown.Markdown(
+        extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+            # 目录扩展(需要加入标记[TOC])
+            'markdown.extensions.toc',
+        ]
+    )
+    article.body = md.convert(article.body)
 
     # 需要传递给模板的对象
     context = {
         'article': article,
+        'toc': md.toc
     }
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
